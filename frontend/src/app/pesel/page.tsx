e94ea2154cfa1b22ca54d112516481059a381fdf
Mateusz Karłowski <mateusz.karlowski@zoho.com>
2023-10-01 00:15:22 +0200

    Added pesel inputs validation and button disable

--- frontend/src/app/pesel/page.tsx
+++ frontend/src/app/pesel/page.tsx
@@ -5,23 +5,40 @@ import Input from "@/app/components/Input/Input";
 import "./pesel.css"
 
 export default function Home(props: any): React.ReactElement {
+    const [pesel, setPesel] = React.useState("")
+    const [pin, setPin] = React.useState("")
+    function buttonDisableCheck(): boolean {
+        return !(pesel.length === 11 && pin.length === 4)
+    }
+
     return (
         <>
             <AnimatedLogo/>
             <h1>Podaj pesel oraz kod recepty</h1>
             <div className="height">
-                <Input placeholder="PESEL" onChange={() => {
-                }}/>
+                <Input placeholder="PESEL" value={pesel} onChange={(value: string) => {
+                    if (value.length > 11)
+                        setPesel(value.slice(0, 11))
+                    else if (!isNaN(Number(value)))
+                        setPesel(value)
+                }
+                }/>
             </div>
             <div className="height">
-                <Input placeholder="PIN" onChange={() => {
-                }}/>
+                <Input placeholder="PIN" type="password" value={pin} onChange={(value: string) => {
+                    if (value.length > 4)
+                        setPin(value.slice(0, 4))
+                    else if (!isNaN(Number(value)))
+                        setPin(value)
+                }
+                }/>
             </div>
-            <button className="button" onClick={buttonHandler}>STWÓRZ TERMINARZ</button>
+            <button className="button" onClick={buttonHandler} disabled={buttonDisableCheck()}>STWÓRZ TERMINARZ</button>
         </>
     )
 }
 
+
 function buttonHandler() {
-    console.log("Button clicked");
+
 }
